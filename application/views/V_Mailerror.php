66322c1c287042c8b84628b8f955d60990df3084
manuelfmora <mfmoradaw@gmail.com>
2016-05-18 14:03:38 +0200

    Creando Correo Contraseña2

---
+++ application/views/V_Mailerror.php
@@ -0,0 +1,16 @@
+  <!-- Correo error -->
+  <section id="aa-error">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="aa-error-area">
+            <h2>ERROR..</h2>
+            <span>No se ha podido enviar el mail.</span>
+            <p>Inténtelo transcurridos unos minutos.</p>
+            <a href="<?= base_url().'index.php'?>"> Ir a la página de inicio.</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- / Correo error -->
---
+++ application/views/V_Mailok.php
@@ -0,0 +1,16 @@
+  <!-- Correo OK -->
+  <section id="aa-error">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="aa-error-area">
+            <h2>Ok</h2>
+            <span>El envio se ha realizado correctamente.</span>
+            <p>Pronto recibira una respuesta.</p>
+            <a href="<?= base_url().'index.php'?>"> Ir a la página de inicio.</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- / Correo OK -->
